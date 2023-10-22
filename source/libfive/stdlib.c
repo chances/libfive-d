c7e2fcc948d716abcae6968200e1d3270ebffbfb
Chance Snow <git@chancesnow.me>
2023-10-21 22:16:04 -0500

    Fix compilation on macOS

--- source/libfive/stdlib.c
+++ source/libfive/stdlib.c
@@ -1,4 +1,6 @@
 // See `dflags` in dub.json for C preprocessor flags:
 // -I subprojects/libfive/libfive/include
 // -I subprojects/libfive/libfive/stdlib
+// FIXME: This was fixed by https://github.com/dlang/dmd/pull/15640
+#define __signed signed
 #include "libfive_stdlib.h"
